86668370ef39acfaa49811634840f12072e1c5be
ldj <tovin4613@gmail.com>
2023-12-09 19:19:25 +0900

    api.py 추가

---
+++ chat/api.py
@@ -0,0 +1,23 @@
+import requests
+
+class CompletionExecutor:
+    def __init__(self, host, api_key, api_key_primary_val, request_id):
+        self._host = host
+        self._api_key = api_key
+        self._api_key_primary_val = api_key_primary_val
+        self._request_id = request_id
+
+    def execute(self, completion_request):
+        headers = {
+            'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
+            'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'text/event-stream'
+        }
+
+        with requests.post(self._host + '/testapp/v1/chat-completions/HCX-002',
+                           headers=headers, json=completion_request, stream=True) as r:
+            for line in r.iter_lines():
+                if line:
+                    print(line.decode("utf-8"))
